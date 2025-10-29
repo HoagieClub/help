0b9f00acfa2453c8e4097eb2891af4d32184687d
david.weizhong.liu@gmail.com <david.weizhong.liu@gmail.com>
2025-10-29 14:08:39 -0400

    david page?

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -70,9 +70,9 @@ const teamLeads: member[] = [
 const teamMembers = [
 	// Add your name, role, and image here!
 	{
-		name: 'Hoagie Member',
-		role: 'Frontend Developer',
-		imgSrc: 'https://github.com/HoagieClub/club/raw/refs/heads/source/src/assets/hoagie-hi.svg',
+		name: 'David Liu',
+		role: 'Developer',
+		imgSrc: 'https://media.licdn.com/dms/image/v2/D4E03AQHBROhhswIn4g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1728969894574?e=1763596800&v=beta&t=01lDxnJDUYtJ6tmMR1JQ9bCmycdyv0rZ7u5w4XnRer8',
 		socials: {
 			linkedin: 'https://www.linkedin.com',
 		},
@@ -172,8 +172,8 @@ export function App() {
 									src={member.imgSrc}
 									alt={member.name}
 									className='w-24 h-24 rounded-full mx-auto mb-4 border-4 border-slate-200 group-hover:border-emerald-300 transition-colors duration-300'
-									height={0}
-									width={0}
+									height={128}
+									width={128}
 								/>
 								<h4 className='font-bold text-slate-800 text-lg'>{member.name}</h4>
 								<p className='text-emerald-600 text-sm'>{member.role}</p>
