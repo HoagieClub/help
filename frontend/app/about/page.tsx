a113066b189b9087fee71f97e537b5354fb0cf24
david.weizhong.liu@gmail.com <david.weizhong.liu@gmail.com>
2025-10-29 20:44:29 -0400

    merged

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -70,11 +70,6 @@ const teamLeads: member[] = [
 const teamMembers = [
 	// Add your name, role, and image here!
 	{
-		name: 'David Liu',
-		role: 'Developer',
-		imgSrc: 'https://media.licdn.com/dms/image/v2/D4E03AQHBROhhswIn4g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1728969894574?e=1763596800&v=beta&t=01lDxnJDUYtJ6tmMR1JQ9bCmycdyv0rZ7u5w4XnRer8',
-		socials: {
-			linkedin: 'https://www.linkedin.com/in/david-liu-71398523a/',
 		name: 'Timothy Li',
 		role: 'Frontend Developer',
 		imgSrc: 'https://avatars.githubusercontent.com/u/214003676?v=4',
@@ -87,6 +82,9 @@ const teamMembers = [
 		name: 'David Liu',
 		role: 'Developer',
 		imgSrc: 'https://media.licdn.com/dms/image/v2/D4E03AQHBROhhswIn4g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1728969894574?e=1763596800&v=beta&t=01lDxnJDUYtJ6tmMR1JQ9bCmycdyv0rZ7u5w4XnRer8',
+		socials: {
+			linkedin: 'https://www.linkedin.com/in/david-liu-71398523a/',
+		},
 	},
 	{
 		name: 'Maribel Crespo',
