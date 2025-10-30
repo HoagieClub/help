2f9dd224af51423e50de845312499497144f641d
davidweizhongliu <96628470+davidweizhongliu@users.noreply.github.com>
2025-10-30 01:57:39 -0400

    DEV-346 David contributor (#14)
    * david name and picture and role
    
    * Revert "david name and picture and role"
    
    This reverts commit cbe1f76fb5504f1eb0074f8bbb300dba7ad32968.
    
    * david page?
    
    * Revert "david name and picture and role"
    
    This reverts commit cbe1f76fb5504f1eb0074f8bbb300dba7ad32968.
    
    * Revert "david name and picture and role"
    
    This reverts commit cbe1f76fb5504f1eb0074f8bbb300dba7ad32968.
    
    * working 22.9.0 version, tsconfig was automatically reconfigured by terminal for some reason, but it doesn't seem to have changed any actual content
    
    * prettier
    
    * changes
    
    * merged
    
    * tsconfig change

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -86,6 +86,14 @@ const teamMembers = [
 		},
 	},
 	{
+		name: 'David Liu',
+		role: 'Developer',
+		imgSrc: 'https://media.licdn.com/dms/image/v2/D4E03AQHBROhhswIn4g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1728969894574?e=1763596800&v=beta&t=01lDxnJDUYtJ6tmMR1JQ9bCmycdyv0rZ7u5w4XnRer8',
+		socials: {
+			linkedin: 'https://www.linkedin.com/in/david-liu-71398523a/',
+		},
+	},
+	{
 		name: 'Maribel Crespo',
 		role: 'Developer',
 		imgSrc: 'https://media.licdn.com/dms/image/v2/D5603AQHOE2hLdzPWhQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1730483078731?e=2147483647&v=beta&t=BgVPzlj748EpPYDDtjMW2NBQpQamT5--CWHtLWh9xHM',
