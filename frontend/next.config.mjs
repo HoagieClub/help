8e2a4528fc60725b18b6935e578b8472a103d4f0
Spencer04Hire <124114480+Spencer04Hire@users.noreply.github.com>
2025-10-29 20:47:52 -0400

    Add github avatars domain

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -19,6 +19,10 @@ const nextConfig = {
 				protocol: 'https',
 				hostname: 'media.licdn.com',
 			},
+			{
+				protocol: 'https',
+				hostname: 'avatars.githubusercontent.com',
+			},
 		],
 	},
 };
