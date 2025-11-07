deb4f24bfd914a21cce73837f0314e0833ea3ccd
charlespers <charlespers.m@gmail.com>
2025-11-07 11:21:20 -0500

    Lint fix pt.2

--- frontend/components/search/hooks/useSearchPosts.ts
+++ frontend/components/search/hooks/useSearchPosts.ts
@@ -4,9 +4,9 @@
 
 import { useMemo } from 'react';
 
-import { PostProps } from 'frontend/components/ui/Post';
+import type { PostProps } from 'frontend/components/ui/Post';
 
-export const useSearchPosts = (posts: PostProps[], query: String): PostProps[] => {
+export const useSearchPosts = (posts: PostProps[], query: string): PostProps[] => {
 	const filtered = useMemo(() => {
 		const q = query.toLowerCase().trim();
 		if (!q) return posts;
