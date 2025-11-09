cdfa313061c7400f3c1c43859ca452f54fcb7824
Spencer04Hire <124114480+Spencer04Hire@users.noreply.github.com>
2025-11-08 22:51:29 -0500

    Move django project to config directory

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'hoagiehelp.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'hoagiehelp.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
