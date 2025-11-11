ddf73762e61795d5037e6ed570ffa9b2f0f2c204
ericguo202 <eric.guo171@gmail.com>
2025-11-11 16:38:33 -0500

    add comment model

---
+++ backend/hoagiehelp/models/comment.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    answer = models.ForeignKey('Answer', on_delete=models.CASCADE)
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+    user = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
+    text = models.TextField(blank=False)
+    hearts = models.PositiveIntegerField(default=0)
+    is_anonymous = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'Comment'
+
+    def __str__(self):
+        return f'Comment on {self.answer_id} by {self.user_id}'
