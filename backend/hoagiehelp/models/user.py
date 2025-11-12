c2d3049012a2698de6fff602cd3896011af7d21b
ericguo202 <eric.guo171@gmail.com>
2025-11-11 23:02:56 -0500

    fix table name

--- backend/hoagiehelp/models/user.py
+++ backend/hoagiehelp/models/user.py
@@ -16,7 +16,7 @@ class CustomUser(AbstractUser):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = "users"
+        db_table = "User"
 
     def __str__(self):
         return f"{self.get_full_name()} ({self.net_id})"
