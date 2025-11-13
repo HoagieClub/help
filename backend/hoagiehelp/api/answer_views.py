11a3961ae9cc3adc04429d6909be2c10fffac712
charlespers <charlespers.m@gmail.com>
2025-11-13 16:25:21 -0500

    restore answer_view.py from preview to exclude from PR'

--- backend/hoagiehelp/api/answer_views.py
+++ backend/hoagiehelp/api/answer_views.py
@@ -1,9 +1,6 @@
 from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status
-
-from .models import Answer
 
 
 class AnswerSerializer(serializers.ModelSerializer):
@@ -27,31 +24,12 @@ class AnswerDetailView(APIView):
 
     def get(self, request, answer_id: str) -> Response:
         """Get all details associated with a given answer."""
-        try:
-            answer = Answer.objects.get(id=answer_id)
-        except Answer.DoesNotExist:
-            return Response({"detail": "Answer not found"}, status=status.HTTP_404_NOT_FOUND)
-        serializer = AnswerSerializer(answer)
-        return Response(serializer.data)
+        pass
 
     def put(self, request, answer_id: str) -> Response:
         """Update an existing answer."""
-        try:
-            answer = Answer.objects.get(id=answer_id)
-        except Answer.DoesNotExist:
-            return Response({"detail": "Answer not found"}, status=status.HTTP_404_NOT_FOUND)
-        serializer = AnswerSerializer(answer, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+        pass
 
     def delete(self, request, answer_id: str) -> Response:
         """Delete an existing answer."""
-        try:
-            answer = Answer.objects.get(id=answer_id)
-        except Answer.DoesNotExist:
-            return Response({"detail":"Answer not found"}, status=status.HTTP_404_NOT_FOUND)
-        answer.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        pass
