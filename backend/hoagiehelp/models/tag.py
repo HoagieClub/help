ae02339b4ddeb5c69aec42064a6d84617c79d61c
rayk <mrraykong@gmail.com>
2025-11-12 20:48:33 -0500

    added tag model (tag.py), containing a choice field and id

---
+++ backend/hoagiehelp/models/tag.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Choices for question_tag
+QUESTION_TAG_CHOICES = (
+        (1, "exam prep"),
+        (2, "problem set"),
+        (3, "study tips"),
+        (4, "conceptual"),
+        (5, "course selection"),
+        (6, "degree planning"),
+        (7, "citation"),
+        (8, "course advice"),
+        (9, "career"),
+    )
+
+class Tag(models.Model):
+    '''Represents a tag on a question'''
+    
+    id = models.AutoField(primary_key=True)
+    tag = models.IntegerField(choices=QUESTION_TAG_CHOICES, db_index=True, null=True)
+
+    class Meta:
+        db_table = 'Tag'
+
+    def __str__(self):
+        return f'Question has tags: {self.tag}'
