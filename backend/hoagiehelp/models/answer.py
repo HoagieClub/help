dd19476b33a886b328098219c528f96cde0fb8e2
rayk <mrraykong@gmail.com>
2025-11-18 07:12:44 -0500

    field question_id to question

--- backend/hoagiehelp/models/answer.py
+++ backend/hoagiehelp/models/answer.py
@@ -4,7 +4,7 @@ class Answer(models.Model):
     """Represents an answer to a question"""
     
     id = models.AutoField(primary_key=True)
-    question_id = models.ForeignKey('Question', on_delete=models.CASCADE)
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
     user = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
     text = models.TextField(blank=False)
     hearts = models.PositiveIntegerField(default=0)
@@ -16,4 +16,4 @@ class Answer(models.Model):
         db_table = 'Answer'
 
     def __str__(self):
-        return f'Answer to question {self.question_id} by {self.user.net_id}'
+        return f'Answer to question {self.question} by {self.user.net_id}'
