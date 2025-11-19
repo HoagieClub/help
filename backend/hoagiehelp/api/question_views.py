7a1201b5c45c2bcf4ccde14133bbaf07925467e8
issacli-0821 <tl1719@princeton.edu>
2025-11-19 02:41:29 -0500

    Update import path

--- backend/hoagiehelp/api/question_views.py
+++ backend/hoagiehelp/api/question_views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
-from models import Question
+from models.question import Question
 
 
 class QuestionSerializer(serializers.ModelSerializer):
