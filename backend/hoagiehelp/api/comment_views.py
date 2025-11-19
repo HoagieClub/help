ba0d0fd52f52467089c83b5bd0d13a5acf3d9c2f
issacli-0821 <tl1719@princeton.edu>
2025-11-19 14:36:51 -0500

    Update import for Comment model

--- backend/hoagiehelp/api/comment_views.py
+++ backend/hoagiehelp/api/comment_views.py
@@ -3,7 +3,7 @@ from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from ..models.comment import Comment
+from hoagiehelp.models.comment import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
