87221c7533302f3b4b9b2f8198d3513ee21850a1
issacli-0821 <tl1719@princeton.edu>
2025-11-23 14:22:06 -0500

    Fix import in answer_views.py

--- backend/hoagiehelp/api/answer_views.py
+++ backend/hoagiehelp/api/answer_views.py
@@ -2,7 +2,7 @@ from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from hoagiehelp.models import Answer
+from hoagiehelp.models.answer import Answer
 
 
 class AnswerSerializer(serializers.ModelSerializer):
