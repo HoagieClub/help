5f494dbc371440d907ecc82459098d89bc4cdfed
timothyli1331 <timothyli1331@gmail.com>
2025-11-24 00:46:17 -0500

    Changed layout.tsx to include ref to StudyGroups instead of feature2

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -50,7 +50,7 @@ async function Content({ children }: ContentProps): Promise<JSX.Element> {
 	const tabs = [
 		{ title: 'About', href: '/about' },
 		{ title: 'Feature 1', href: '/feature1' },
-		{ title: 'Feature 2', href: '/feature2' },
+		{ title: 'Study Groups', href: '/StudyGroups' }, 
 		{ title: 'Feature 3', href: '/feature3' },
 	];
 
