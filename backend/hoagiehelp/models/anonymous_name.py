6a4934573123576117509ab5bd626ab1f08cfced
Spencer04Hire <124114480+Spencer04Hire@users.noreply.github.com>
2025-12-03 16:57:33 -0500

    Fix up model and lint

--- backend/hoagiehelp/models/anonymous_name.py
+++ backend/hoagiehelp/models/anonymous_name.py
@@ -1,27 +1,29 @@
 from django.db import models
 from django.db.models import Q
 
+from hoagiehelp.models.question import Question
+from hoagiehelp.models.user import CustomUser
 
-class Anonymous_Name(models.Model):
+
+class AnonymousName(models.Model):
     """Model to store anonymous names associated with users and questions."""
 
     id = models.AutoField(primary_key=True)
     anonymous_name = models.CharField(max_length=255)
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
                 fields=["question", "user"],
                 name="unique_question_user",
-                condition=Q(user__isnull=False),
             ),
             models.UniqueConstraint(
                 fields=["question", "anonymous_name"],
                 name="unique_question_anonymous_name",
-                condition=Q(anonymous_name__isnull=False),
             ),
         ]
+
     def __str__(self):
         return f"Anonymous_Name(id={self.id}, anonymous_name={self.anonymous_name})"
--- backend/hoagiehelp/models/question.py
+++ backend/hoagiehelp/models/question.py
@@ -1,13 +1,17 @@
-from django.db import models
 from django.core.validators import MinValueValidator
+from django.db import models
+
+from hoagiehelp.models.tag import Tag
+from hoagiehelp.models.user import CustomUser
+
 
 class Question(models.Model):
     """Represents a question posted by a user."""
-    
-    id = models.AutoField(primary_key = True)
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    tags = models.ManyToManyField('Tag', related_name='questions')
+    tags = models.ManyToManyField(Tag)
     course = models.CharField(max_length=100, blank=True, null=True, db_index=True)
     details = models.TextField()
     create_time = models.DateTimeField(auto_now_add=True)
