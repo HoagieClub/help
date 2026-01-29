471c801b32d63a1b6c03200a1bf2766021a80144
issacli-0821 <tl1719@princeton.edu>
2026-01-28 20:55:16 -0500

    Lint studygroup.py

--- backend/hoagiehelp/models/studygroup.py
+++ backend/hoagiehelp/models/studygroup.py
@@ -1,13 +1,13 @@
-from django.db import models
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator
+from django.db import models
 
 User = get_user_model()
 MAX_TITLE_LENGTH = 255
 
 
 class StudyGroup(models.Model):
-    '''Represents a study group.'''
+    """Represents a study group."""
 
     title = models.CharField(max_length=MAX_TITLE_LENGTH)
     description = models.TextField(blank=True)
@@ -19,10 +19,8 @@ class StudyGroup(models.Model):
     )
 
     meeting_datetime = models.DateTimeField(db_index=True)
-    max_spots = models.PositiveIntegerField(
-        validators=[MinValueValidator(1)]
-    )
-    
+    max_spots = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+
     members = models.ManyToManyField(
         User,
         related_name="study_groups_joined",
@@ -33,29 +31,28 @@ class StudyGroup(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ["-created_at"]
         constraints = [
-        models.UniqueConstraint(
-            fields=['leader', 'meeting_datetime', 'title'],
-            name='unique_study_group_per_leader'
-        )
-    ]
+            models.UniqueConstraint(
+                fields=["leader", "meeting_datetime", "title"],
+                name="unique_study_group_per_leader",
+            )
+        ]
 
     @property
     def spots_taken(self):
-        '''Returns number of members'''
+        """Returns number of members"""
         return self.members.count()
 
     @property
     def spots_remaining(self):
-        '''Returns remaining spots'''
+        """Returns remaining spots"""
         return max(self.max_spots - self.spots_taken, 0)
 
     @property
     def is_full(self):
-        '''Check if the study group is full'''
+        """Check if the study group is full"""
         return self.spots_taken >= self.max_spots
 
     def __str__(self):
         return self.title
-
