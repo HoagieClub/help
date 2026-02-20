8071078423570dc14f377297f2eb593254de2ff7
Hellen Luo <luohellen05@gmail.com>
2026-02-20 15:30:32 -0500

    changed div to Pane and removed threads

--- frontend/components/CommentsPanel.tsx
+++ frontend/components/CommentsPanel.tsx
@@ -2,6 +2,8 @@
 
 import React from 'react';
 
+import { Pane } from 'evergreen-ui';
+
 import { CommentBox } from './CommentBox';
 
 import type { Comment } from '../api/commentService';
@@ -10,38 +12,15 @@ type CommentsPanelProps = {
 	comments: Comment[];
 };
 
-const getThreadedComments = (comments: Comment[]) => {
-	const threads: Record<number, Comment[]> = {};
-	comments.forEach((comment) => {
-		if (!threads[comment.answer]) {
-			threads[comment.answer] = [];
-		}
-		threads[comment.answer].push(comment);
-	});
-	return Object.values(threads);
-};
-
-const Thread: React.FC<{ comments: Comment[] }> = ({ comments }) => {
-	return (
-		<div className='thread'>
-			{comments.map((comment, idx) => (
-				<div key={comment.id}>
-					<CommentBox comment={comment} showThreadLine={idx < comments.length - 1} />
-				</div>
-			))}
-		</div>
-	);
-};
-
 const CommentsPanel: React.FC<CommentsPanelProps> = ({ comments }) => {
-	const threads = getThreadedComments(comments);
-
 	return (
-		<div className='comments-panel flex flex-col gap-6'>
-			{threads.map((threadComments) => (
-				<Thread comments={threadComments} key={threadComments[0].answer} />
+		<Pane className='comments-panel flex flex-col gap-6'>
+			{comments.map((comment, idx) => (
+				<Pane key={comment.id}>
+					<CommentBox comment={comment} showThreadLine={idx < comments.length - 1} />
+				</Pane>
 			))}
-		</div>
+		</Pane>
 	);
 };
 
