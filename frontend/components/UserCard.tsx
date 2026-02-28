c21a2165e5f9d9bad5b902d1c6a9995228c68fa4
timothyli1331 <timothyli1331@gmail.com>
2026-02-28 01:03:43 -0500

    Made changes and changed stuff to Evergreen

--- frontend/components/UserCard.tsx
+++ frontend/components/UserCard.tsx
@@ -1,30 +1,41 @@
 'use client';
 
-import { Pane } from 'evergreen-ui';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
-type User = {
+import { Text, Card, majorScale, minorScale, Pane } from 'evergreen-ui';
+
+type UserCardProps = {
 	net_id: string;
 	class_year: number;
 	hearts: number;
 	created_at: string;
 };
 
-function UserCard({ user }: { user: User }) {
+function UserCard({ user }: { user: UserCardProps }) {
 	return (
-		<Pane
+		<Card
 			border='default'
-			borderRadius={8}
-			padding={16}
+			borderRadius={minorScale(1)}
+			padding={majorScale(2)}
 			maxWidth={320}
 			marginX='auto'
-			marginY={20}
+			marginY={majorScale(2)}
 			background='white'
 		>
-			<p>{user.net_id}</p>
-			<p>Class Year: {user.class_year}</p>
-			<p>Hearts: {user.hearts}</p>
-			<p>Joined: {new Date(user.created_at).toLocaleDateString()}</p>
-		</Pane>
+			<Pane display='flex' flexDirection='column' gap={minorScale(1)}>
+				<Text size={500} fontWeight={600}>
+					{user.net_id}
+				</Text>
+
+				<Text size={400}>Class Year: {user.class_year}</Text>
+				<Text size={400}>
+					{' '}
+					<FavoriteIcon fontSize='small' sx={{ color: 'red' }} /> Hearts{' '}
+					<FavoriteIcon fontSize='small' sx={{ color: 'red' }} />: {user.hearts}
+				</Text>
+				<Text size={400}>Joined: {new Date(user.created_at).toLocaleDateString()}</Text>
+			</Pane>
+		</Card>
 	);
 }
 
