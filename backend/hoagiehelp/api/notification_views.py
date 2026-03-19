09f85813d3ffe6ad841a3a1e15891365048edf55
rayk <mrraykong@gmail.com>
2026-03-18 20:07:39 -0400

    - Marked notification_views fields as read only
    - Updated notification post method to update notifications as opposed to create

--- backend/hoagiehelp/api/notification_views.py
+++ backend/hoagiehelp/api/notification_views.py
@@ -17,6 +17,14 @@ class NotificationSerializer(serializers.ModelSerializer):
             "is_read",
             "created_at",
         ]
+        read_only_fields = [
+            "id",
+            "user",
+            "question",
+            "answer",
+            "comment",
+            "created_at"
+        ]
 
 class NotificationView(APIView):
     """Handle collection operations for notifications."""
@@ -35,12 +43,13 @@ class NotificationView(APIView):
         notification.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    def post(self, request) -> Response:
-        """Create a new notification."""
-        serializer = NotificationSerializer(data=request.data)
+    def post(self, request, notification_id=None) -> Response:
+        """Update an existing notification."""
+        notification = get_object_or_404(Notification, id=notification_id, user=request.user)
+        serializer = NotificationSerializer(notification, data=request.data, partial=True)
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
