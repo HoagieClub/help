1eee7fd35958f8466b756a6447b3ba3653c1f9f5
adambelouad <132090786+adambelouad@users.noreply.github.com>
2026-03-18 20:52:29 -0400

    Remove user-preview page and sampleData from PR

--- frontend/app/user-preview/page.tsx
+++
@@ -1,6 +0,0 @@
-import { SAMPLE_USER_POSTS_PROPS } from '@/components/user/sampleData';
-import UserPostsPage from '@/components/user/UserPostsPage';
-
-export default function UserPreviewPage() {
-	return <UserPostsPage {...SAMPLE_USER_POSTS_PROPS} />;
-}
