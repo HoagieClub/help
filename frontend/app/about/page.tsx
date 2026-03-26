9901484fad8f41fe778f41c39a8a536a4fc0028a
timothyli1331 <timothyli1331@gmail.com>
2026-03-25 20:05:05 -0400

    Fixed the about page

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -22,7 +22,7 @@ const SocialIcon = ({ href, children }: { href: string; children: React.ReactNod
 		href={href}
 		target='_blank'
 		rel='noopener noreferrer'
-		className='text-gray-400 hover:text-emerald-500 transition-colors duration-300'
+		className='text-gray-400 hover:text-red-500 transition-colors duration-300'
 	>
 		{children}
 	</a>
@@ -263,7 +263,7 @@ export function App() {
 										<h3 className='text-2xl font-bold text-slate-900'>
 											{lead.name}
 										</h3>
-										<p className='text-md font-semibold text-emerald-600 mb-2'>
+										<p className='text-md font-semibold text-red-600 mb-2'>
 											{lead.role}
 										</p>
 										<div className='flex justify-center sm:justify-start space-x-4'>
@@ -292,13 +292,13 @@ export function App() {
 								<Image
 									src={member.imgSrc}
 									alt={member.name}
-									className='w-24 h-24 rounded-full mx-auto mb-4 border-4 border-slate-200 group-hover:border-emerald-300 transition-colors duration-300'
+									className='w-24 h-24 rounded-full mx-auto mb-4 border-4 border-slate-200 group-hover:border-red-300 transition-colors duration-300'
 									height={128}
 									width={128}
 									style={{ objectFit: 'cover' }}
 								/>
 								<h4 className='font-bold text-slate-800 text-lg'>{member.name}</h4>
-								<p className='text-emerald-600 text-sm'>{member.role}</p>
+								<p className='text-red-600 text-sm'>{member.role}</p>
 								<div className='flex mx-auto w-min mt-2 justify-center sm:justify-start space-x-4'>
 									<SocialIcon href={member.socials.linkedin}>
 										<LinkedinIcon />
