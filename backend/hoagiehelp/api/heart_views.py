95d5b0d4938b191604a12a19aadefaecf76d80c3
issacli-0821 <tl1719@princeton.edu>
2026-03-28 03:22:27 -0400

    Pass CustomUser directly from request

--- backend/hoagiehelp/api/heart_views.py
+++ backend/hoagiehelp/api/heart_views.py
@@ -10,19 +10,15 @@ from hoagiehelp.models.heart import Heart
 from hoagiehelp.models.question import Question
 from hoagiehelp.models.answer import Answer
 from hoagiehelp.models.comment import Comment
-from hoagiehelp.models.user import CustomUser
-
 
 class HeartView(APIView):
 
     @transaction.atomic
     def heart_question(self, request, question_id: int):
-        net_id = request.user.net_id
-        user_obj = get_object_or_404(CustomUser, net_id=net_id)
         question = get_object_or_404(Question, id=question_id)
 
         # If already hearted, delete the heart record from the db and decrement the hearts count
-        heart = Heart.objects.filter(question=question, user=user_obj)
+        heart = Heart.objects.filter(question=question, user=request.user)
         if heart.exists():
             heart.delete()
             question.hearts = F('hearts') - 1
@@ -32,7 +28,7 @@ class HeartView(APIView):
 
         # otherwise, create a new heart record and increment the hearts count
         else:
-            Heart.objects.create(question=question, user=user_obj)
+            Heart.objects.create(question=question, user=request.user)
             question.hearts = F('hearts') + 1
             question.save()
             question.refresh_from_db()
@@ -40,11 +36,9 @@ class HeartView(APIView):
 
     @transaction.atomic
     def heart_answer(self, request, answer_id: int):
-        net_id = request.user.net_id
-        user_obj = get_object_or_404(CustomUser, net_id=net_id)
         answer = get_object_or_404(Answer, id=answer_id)
 
-        heart = Heart.objects.filter(answer=answer, user=user_obj)
+        heart = Heart.objects.filter(answer=answer, user=request.user)
         if heart.exists():
             heart.delete()
             answer.hearts = F('hearts') - 1
@@ -52,7 +46,7 @@ class HeartView(APIView):
             answer.refresh_from_db()
             return Response({"hearts": answer.hearts, "is_hearted": False}, status=status.HTTP_200_OK)
         else:
-            Heart.objects.create(answer=answer, user=user_obj)
+            Heart.objects.create(answer=answer, user=request.user)
             answer.hearts = F('hearts') + 1
             answer.save()
             answer.refresh_from_db()
@@ -60,11 +54,9 @@ class HeartView(APIView):
 
     @transaction.atomic
     def heart_comment(self, request, comment_id: int):
-        net_id = request.user.net_id
-        user_obj = get_object_or_404(CustomUser, net_id=net_id)
         comment = get_object_or_404(Comment, id=comment_id)
 
-        heart = Heart.objects.filter(comment=comment, user=user_obj)
+        heart = Heart.objects.filter(comment=comment, user=request.user)
         if heart.exists():
             heart.delete()
             comment.hearts = F('hearts') - 1
@@ -72,7 +64,7 @@ class HeartView(APIView):
             comment.refresh_from_db()
             return Response({"hearts": comment.hearts, "is_hearted": False}, status=status.HTTP_200_OK)
         else:
-            Heart.objects.create(comment=comment, user=user_obj)
+            Heart.objects.create(comment=comment, user=request.user)
             comment.hearts = F('hearts') + 1
             comment.save()
             comment.refresh_from_db()
