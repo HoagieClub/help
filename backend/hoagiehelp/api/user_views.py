a57b4ebb0e1f6b950a417530ef04307d17a8219f
Ethan Wen <75800128+wen-ethan@users.noreply.github.com>
2026-04-03 16:34:23 -0400

    add code to grab CustomUser object and filter for all relevant comments, then serialize

--- backend/hoagiehelp/api/user_views.py
+++ backend/hoagiehelp/api/user_views.py
@@ -1,6 +1,10 @@
-from rest_framework import serializers
+from django.shortcuts import get_object_or_404
+from rest_framework import serializers, status
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from hoagiehelp.api.comment_views import CommentSerializer
+from hoagiehelp.models.comment import Comment
 from hoagiehelp.models.user import CustomUser
 
 
@@ -31,5 +35,16 @@ def user_answers(request, user_id: str):
     pass
 
 
-def user_comments(request, user_id: str):
-    pass
+def user_comments(request, user_id: str) -> Response:
+    # retrieve all comments a user has posted
+    get_object_or_404(CustomUser, net_id=user_id)
+
+    queryset = Comment.objects.filter(user__net_id=user_id).order_by("-created_at")
+
+    if user_id != request.user.net_id:
+        # only show public comments for other users
+        queryset = queryset.filter(is_anonymous=False)
+
+    serializer = CommentSerializer(queryset, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
