a3a9ced086672f56a7480d71e2848432870732dc
raykong06 <114007162+raykong06@users.noreply.github.com>
2026-04-09 21:21:31 -0400

    DEV-555: Add get_questions_for_user to UserView (#114)
    * Added get_questions_for_user to UserView
    
    * moved get_questions_for_user outside UserViews
    
    * Change get_questions_for_user variable names and parameter to match route's parameter.
    
    Corrected user_id matching.
    
    * Eliminated unnecessary empty lines.
    
    ---------
    
    Co-authored-by: Spencer Doyle <124114480+Spencer04Hire@users.noreply.github.com>

--- backend/hoagiehelp/api/user_views.py
+++ backend/hoagiehelp/api/user_views.py
@@ -6,7 +6,8 @@ from rest_framework.views import APIView
 from hoagiehelp.api.comment_views import CommentSerializer
 from hoagiehelp.models.comment import Comment
 from hoagiehelp.models.user import CustomUser
-
+from hoagiehelp.models.question import Question
+from backend.hoagiehelp.api.question_views import QuestionSerializer
 
 # User Serializer
 class UserSerializer(serializers.ModelSerializer):
@@ -26,10 +27,16 @@ class UserSerializer(serializers.ModelSerializer):
 class UserView(APIView):
     pass
 
+def get_questions_for_user(request, user_id: str):
+    target_user = get_object_or_404(CustomUser, id=user_id)
+    questions_set = Question.objects.filter(user=target_user)
 
-def user_questions(request, user_id: str):
-    pass
-
+    if request.user.net_id != target_user.net_id:
+        questions_set = questions_set.filter(user_is_anonymous=False)
+    
+    questions_set = questions_set.order_by("-created_at")
+    serializer = QuestionSerializer(questions_set, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 def user_answers(request, user_id: str):
     pass
