1b67931933d46d73cbb2a15d950db63441253c87
issacli-0821 <tl1719@princeton.edu>
2026-04-15 02:16:55 -0400

    Remove copyright text in CommentForm

--- frontend/components/comment/CommentForm.tsx
+++ frontend/components/comment/CommentForm.tsx
@@ -1,15 +1,3 @@
-/**
- * @overview CommentForm component for creating new comments on an answer.
- *
- * Copyright © 2021-2025 Hoagie Club and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree or at https://github.com/hoagieclub/help/LICENSE.
- *
- * Permission is granted under the MIT License to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the software. This software is provided "as-is", without warranty of any kind.
- */
-
 'use client';
 
 import React, { useState } from 'react';
